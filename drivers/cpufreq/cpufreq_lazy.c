c6b41f78b21ab5818ba75b7f3b8cade1a48f2bd0
franciscofranco <franciscofranco.1990@gmail.com>
2011-10-30 13:36:54 +0000

    Add 2.6.36 ondemand patch to improve performance to lazy

--- drivers/cpufreq/cpufreq_lazy.c
+++ drivers/cpufreq/cpufreq_lazy.c
@@ -35,6 +35,8 @@
 
 #define DEF_FREQUENCY_DOWN_DIFFERENTIAL		(10)
 #define DEF_FREQUENCY_UP_THRESHOLD		(80)
+#define DEF_SAMPLING_DOWN_FACTOR    		(1)
+#define MAX_SAMPLING_DOWN_FACTOR    		(100000)
 #define MICRO_FREQUENCY_DOWN_DIFFERENTIAL	(3)
 #define MICRO_FREQUENCY_UP_THRESHOLD		(90)
 #define MICRO_FREQUENCY_MIN_SAMPLE_RATE		(10000)
@@ -87,6 +89,7 @@ struct cpu_dbs_info_s {
     unsigned int freq_lo;
     unsigned int freq_lo_jiffies;
     unsigned int freq_hi_jiffies;
+    unsigned int rate_mult;
     int cpu;
     unsigned int sample_type:1;
     /*
@@ -113,6 +116,7 @@ static struct dbs_tuners {
     unsigned int up_threshold;
     unsigned int down_differential;
     unsigned int ignore_nice;
+    unsigned int sampling_down_factor;
     unsigned int powersave_bias;
     unsigned int min_timeinstate;
 #ifdef CONFIG_HAS_EARLYSUSPEND
@@ -120,6 +124,7 @@ static struct dbs_tuners {
 #endif
 } dbs_tuners_ins = {
     .up_threshold = DEF_FREQUENCY_UP_THRESHOLD,
+    .sampling_down_factor = DEF_SAMPLING_DOWN_FACTOR,
     .down_differential = DEF_FREQUENCY_DOWN_DIFFERENTIAL,
     .ignore_nice = 0,
     .powersave_bias = 0,
@@ -293,6 +298,7 @@ define_one_global_ro(sampling_rate_min);
     }
 show_one(sampling_rate, sampling_rate);
 show_one(up_threshold, up_threshold);
+show_one(sampling_down_factor, sampling_down_factor);
 show_one(ignore_nice_load, ignore_nice);
 show_one(powersave_bias, powersave_bias);
 show_one(min_timeinstate, min_timeinstate);
@@ -363,6 +369,29 @@ static ssize_t store_up_threshold(struct kobject *a, struct attribute *b,
     mutex_unlock(&dbs_mutex);
 
     return count;
+}
+
+static ssize_t store_sampling_down_factor(struct kobject *a,
+      struct attribute *b, const char *buf, size_t count)
+{
+  unsigned int input, j;
+  int ret;
+  ret = sscanf(buf, "%u", &input);
+
+  if (ret != 1 || input > MAX_SAMPLING_DOWN_FACTOR || input < 1)
+    return -EINVAL;
+  mutex_lock(&dbs_mutex);
+  dbs_tuners_ins.sampling_down_factor = input;
+
+  /* Reset down sampling multiplier in case it was active */
+  for_each_online_cpu(j) {
+    struct cpu_dbs_info_s *dbs_info;
+    dbs_info = &per_cpu(od_cpu_dbs_info, j);
+    dbs_info->rate_mult = 1;
+  }
+  mutex_unlock(&dbs_mutex);
+
+  return count;
 }
 
 static ssize_t store_ignore_nice_load(struct kobject *a, struct attribute *b,
@@ -459,6 +488,7 @@ static ssize_t store_screenoff_maxfreq(struct kobject *a, struct attribute *b,
 
 define_one_global_rw(sampling_rate);
 define_one_global_rw(up_threshold);
+define_one_global_rw(sampling_down_factor);
 define_one_global_rw(ignore_nice_load);
 define_one_global_rw(powersave_bias);
 define_one_global_rw(min_timeinstate);
@@ -471,6 +501,7 @@ static struct attribute *dbs_attributes[] = {
     &sampling_rate_min.attr,
     &sampling_rate.attr,
     &up_threshold.attr,
+    &sampling_down_factor.attr,
     &ignore_nice_load.attr,
     &powersave_bias.attr,
     &min_timeinstate.attr,
@@ -657,6 +688,10 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 
 	    __cpufreq_driver_target(policy, policy->max,
 				    CPUFREQ_RELATION_H);
+	else if (policy->cur < policy->max)
+			this_dbs_info->rate_mult =
+				dbs_tuners_ins.sampling_down_factor;
+		dbs_freq_increase(policy, policy->max);
 	} else {
 	    int freq = powersave_bias_target(policy, policy->max,
 					     CPUFREQ_RELATION_H);
@@ -684,6 +719,9 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 	freq_next = max_load_freq /
 	    (dbs_tuners_ins.up_threshold -
 	     dbs_tuners_ins.down_differential);
+
+	/* No longer fully busy, reset rate_mult */
+    	    this_dbs_info->rate_mult = 1;
 
 	if (freq_next < policy->min)
 	    freq_next = policy->min;
@@ -739,8 +777,11 @@ static void do_dbs_timer(struct work_struct *work)
 static inline void dbs_timer_init(struct cpu_dbs_info_s *dbs_info)
 {
     /* We want all CPUs to do sampling nearly on same jiffy */
-    int delay = usecs_to_jiffies(current_sampling_rate);
-    delay -= jiffies % delay;
+    int delay = usecs_to_jiffies(dbs_tuners_ins.sampling_rate
+       * dbs_info->rate_mult);
+
+    if (num_online_cpus() > 1)
+    	delay -= jiffies % delay;
 
     dbs_info->sample_type = DBS_NORMAL_SAMPLE;
     INIT_DELAYED_WORK_DEFERRABLE(&dbs_info->work, do_dbs_timer);
@@ -790,6 +831,7 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 	    }
 	}
 	this_dbs_info->cpu = cpu;
+	this_dbs_info->rate_mult = 1;
 	lazy_powersave_bias_init_cpu(cpu);
 	/*
 	 * Start the timerschedule work, when this governor
